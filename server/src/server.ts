9fa2dcbc79c663e8a71861a3457c7afa959778ae
Nasef Ahmed <nasef64@gmail.com>
2026-04-23 11:56:16 +0300

    Ensure configured admin account is active and always admin on startup

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,12 @@ import { createServer } from "http";
 import { createApp } from "./app.js";
 import { connectToDatabase } from "./config/db.js";
 import { env } from "./config/env.js";
+import { ensureAdminAccount } from "./services/ensureAdminAccount.js";
 import { createSocketServer } from "./sockets/index.js";
 
 async function bootstrap() {
   await connectToDatabase();
+  await ensureAdminAccount();
 
   const app = createApp();
   const server = createServer(app);
